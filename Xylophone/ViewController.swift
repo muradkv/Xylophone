13d8671d5c5858a723d2633b51ce2d02afb26737
murad <muradkv@gmail.com>
2019-03-11 15:10:46 +0300

    Add func play sound when notePressed
    Use AVFoundation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        let soundURL = Bundle.main.url(forResource: "noe1", withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            
+        } catch {
+            print("error")
+        }
+        
+        audioPlayer.play()
+    }
+    
 }
 
