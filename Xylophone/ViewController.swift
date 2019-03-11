3e84977a4272c50220560ec7bb08c150ed44b7ef
murad <muradkv@gmail.com>
2019-03-11 16:45:53 +0300

    Del global var, change to input param in function playSound
    Используем параметр в функции soundFileName для передачи имени файла звука из массива звуков soundArray вместо глобальной переменной.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer: AVAudioPlayer!
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
-    var selectedSoundFileName: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +20,11 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-        selectedSoundFileName = soundArray[sender.tag]
-        print(selectedSoundFileName)
-        playSound()
+        playSound(soundFileName: soundArray[sender.tag])
     }
     
-    func playSound() {
-        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+    func playSound(soundFileName: String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
